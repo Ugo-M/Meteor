2805ba762935e4a238ae4fe36c07accfa1e2a544
Ugo-M <ugo.mekki@etu.univ-amu.fr>
2020-01-16 15:12:52 +0000

    stockManager project base
    Signed-off-by: Ugo <ugo.mekki@gmail.com>

---
+++ stockManager/collections/stocks.js
@@ -0,0 +1,35 @@
+Stocks = new Meteor.Collection('stocks');
+
+StockSchema = new SimpleSchema({
+    name: {
+        type: String,
+        label: "Name"
+    },
+    desc: {
+        type: String,
+        label: "Description"
+    },
+    creator: {
+        type: String,
+        label: "Creator",
+        autoValue: function () {
+            return this.userId
+        },
+        autoform: {
+            type: "hidden"
+        }
+    },
+    createdAt: {
+        type: Date,
+        label: "Created At",
+        autoValue: function () {
+            return new Date()
+        },
+        autoform: {
+            type: "hidden"
+        }
+    }
+});
+
+Stocks.attachSchema( StockSchema );
+
